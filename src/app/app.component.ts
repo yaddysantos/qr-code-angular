017e2e392a46427be7aa8e6b7a4c7f710986a43d
yaddysantos <34655963+yaddysantos@users.noreply.github.com>
2022-12-27 11:02:10 -0500

    test for how to use qrcode since angular
    it dependens for version, in angular 11 doesn't work but in 14 works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgxQrcodeElementTypes, NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'qr-code';
+
+  url = 'https://cdn.sanity.io/images/0k2k2bbv/redesign_21/9d8447213a67ce12fcc4ccd186bf2403bbc62052-276x110.png';
+  elementType = NgxQrcodeElementTypes.CANVAS;
+  errorCorrectionLevel = NgxQrcodeErrorCorrectionLevels.LOW;
+  value: string = this.url;
+  
+  qrcode
+
+  /**prueba 2 */
+  public myAngularxQRcode: string = null;
+  public imgIris: string = null;
+
+  constructor(){
+    this.myAngularxQRcode = 'Your QR code data string'
+    this.imgIris = '../assets/img/logo-iris.png'
+  }
+  // public config: Options = {
+  //   width: 300,
+  //   height: 300,
+  //   data: "https://www.facebook.com/",
+  //   image: "https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg",
+  //   margin: 5,
+  //   dotsOptions: {
+  //     color: "#1977f3",
+  //     type: "dots"
+  //   },
+  //   backgroundOptions: {
+  //     color: "#ffffff",
+  //   },
+  //   imageOptions: {
+  //     crossOrigin: "anonymous",
+  //     margin: 0
+  //   }
+  // };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxQRCodeModule } from '@techiediaries/ngx-qrcode';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { QRCodeModule } from 'angularx-qrcode';
+// import { NgxQrcodeStylingModule } from 'ngx-qrcode-styling/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    QRCodeModule,
+    NgxQRCodeModule,
+    // NgxQrcodeStylingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
